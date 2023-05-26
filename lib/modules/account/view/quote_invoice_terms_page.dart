9fa63d3e2a0c8a188565d08a8e3cf7334ecc3751
guri1423 <gurminder2181@gmail.com>
2023-05-26 18:05:15 +0530

    quote invoice

--- lib/modules/account/view/quote_invoice_terms_page.dart
+++ lib/modules/account/view/quote_invoice_terms_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ready_made_4_trade/core/colors.dart';
+import 'package:ready_made_4_trade/modules/customer/pages/customer_page/add_customer.dart';
+import 'package:ready_made_4_trade/modules/home/widgets/common_widgets.dart';
 import 'package:ready_made_4_trade/widgets/bottom_bar_for_all.dart';
 
 class QuoteInvoiceTermsPage extends StatefulWidget {
@@ -10,8 +12,15 @@ class QuoteInvoiceTermsPage extends StatefulWidget {
 }
 
 class _QuoteInvoiceTermsPageState extends State<QuoteInvoiceTermsPage> {
+  final TextEditingController inbox1 = TextEditingController();
+  final TextEditingController inbox2 = TextEditingController();
+  final TextEditingController expiry = TextEditingController();
+  final TextEditingController payments = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+
     return Scaffold(
       backgroundColor: CustomColors.bodyColor,
       appBar: AppBar(
@@ -44,6 +53,133 @@ class _QuoteInvoiceTermsPageState extends State<QuoteInvoiceTermsPage> {
         onBackPress: () {
           Navigator.pop(context);
         },
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          children: [
+            Text(
+              'QUOTE TERMS',
+              style: theme.textTheme.titleLarge!
+                  .copyWith(color: CustomColors.primeColour),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            customTextFieldForm(
+              context,
+              controller: inbox1,
+              isBig: true,
+              hintText:
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed '
+                  'do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                  ' Ut enim ad minim veniam, quis nostrud exercitation ullamco'
+                  ' laboris nisi ut aliquip ex ea commodo consequat. Duis aute '
+                  'irure dolor in reprehenderit in',
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                Text(
+                  'EXPIRY',
+                  style: theme.textTheme.titleLarge!
+                      .copyWith(color: CustomColors.primeColour),
+                ),
+                const SizedBox(
+                  width: 40,
+                ),
+                SizedBox(
+                  height: 40,
+                  width: 80,
+                  child: customTextFieldForm(
+                    context,
+                    controller: expiry,
+                    hintText: '30',
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Text(
+              'INVOICE TERMS',
+              style: theme.textTheme.titleLarge!
+                  .copyWith(color: CustomColors.primeColour),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            customTextFieldForm(
+              context,
+              controller: inbox2,
+              isBig: true,
+              hintText:
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed '
+                  'do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                  ' Ut enim ad minim veniam, quis nostrud exercitation ullamco'
+                  ' laboris nisi ut aliquip ex ea commodo consequat. Duis aute '
+                  'irure dolor in reprehenderit in',
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                Text(
+                  'PAYMENT TERMS',
+                  style: theme.textTheme.titleLarge!
+                      .copyWith(color: CustomColors.primeColour),
+                ),
+                const SizedBox(
+                  width: 40,
+                ),
+                SizedBox(
+                  height: 40,
+                  width: 80,
+                  child: customTextFieldForm(
+                    context,
+                    controller: payments,
+                    hintText: '30',
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: SizedBox(
+                        height: 40,
+                        child: smallButton(
+                            context, 'SAVE', CustomColors.primeColour, 50)),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: SizedBox(
+                        height: 40,
+                        child: smallButton(
+                            context, 'CANCEL', CustomColors.greyButton, 50)),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+          ],
+        ),
       ),
     );
   }
